5ce08185d1a317f96168789f6255ac3264d8da23
Ahmed Refai <110742430+elref7i@users.noreply.github.com>
2025-04-17 13:32:50 +0200

    test 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ const authPages = [
   "/auth/set-password",
   "/auth/verify-code",
 ];
-const publicPages = ["/home", ...Array.from(authPages)];
+const publicPages = ["/", ...Array.from(authPages)];
 
 const handleI18nRouting = createMiddleware(routing);
 
@@ -68,36 +68,3 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
-
-// import { getToken } from 'next-auth/jwt';
-// import { NextRequest, NextResponse } from 'next/server';
-
-// const authPages = new Set([
-//   '/auth/login',
-//   '/auth/register',
-//   '/auth/forgot-password',
-//   '/auth/set-password',
-//   '/auth/verify-code',
-// ]);
-// const publicPages = new Set(['/home', ...Array.from(authPages)]);
-
-// export default async function middleware(req: NextRequest) {
-//   const token = await getToken({ req });
-
-//   if (publicPages.has(req.nextUrl.pathname)) {
-//     if (!token) return NextResponse.next();
-//     if (authPages.has(req.nextUrl.pathname)) {
-//       const redirectUrl = new URL('/', req.nextUrl.origin);
-//       return NextResponse.redirect(redirectUrl);
-//     }
-//   }
-//   if (token) return NextResponse.next();
-//   const redirectUrl = new URL('/auth/login', req.nextUrl.origin);
-//   return NextResponse.redirect(redirectUrl);
-// }
-
-// export const config = {
-//   matcher: [
-//     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-//   ],
-// };
