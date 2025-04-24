b7b6da491d4585d10cfa5d3cc6bf8596338272a7
Ahmed Refai <110742430+elref7i@users.noreply.github.com>
2025-04-24 23:21:48 +0200

    feat: implement new statistics and subjects components, refactor dashboard structure, and update middleware for public pages

--- src/app/[locale]/auth/login/_hooks/use-login.ts
+++ src/app/[locale]/auth/login/_hooks/use-login.ts
@@ -7,7 +7,7 @@ export default function useLogin() {
   const { isPending, mutate: login } = useMutation({
     mutationFn: async (loginField: LoginFields) => {
       const response = await signIn("credentials", {
-        callbackUrl: "/dashboard",
+        callbackUrl: "/",
         redirect: false,
         email: loginField.email,
         password: loginField.password,
@@ -17,7 +17,7 @@ export default function useLogin() {
     },
     onSuccess: (data) => {
       setTimeout(() => {
-        window.location.href = data?.url || "/dashboard";
+        window.location.href = data?.url || "/";
       }, 1000);
       toast.success("Logged in successfully");
     },
