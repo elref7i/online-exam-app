088cac7a2dc8f5a1b0932ad96b9306a8a4607928
Ahmed Refai <momo3956248@gmail.com>
2025-05-04 17:58:36 +0300

    feat: add support for Vercel auth cookie in authentication headers

--- src/lib/constants/auth-cookie.constant.ts
+++ src/lib/constants/auth-cookie.constant.ts
@@ -1,1 +1,2 @@
 export const AUTH_COOKIE = "next-auth.session-token";
+export const AUTH_COOKIE_VERCEL = "__Secure-next-auth.session-token";
--- src/lib/utils/auth-headers.ts
+++ src/lib/utils/auth-headers.ts
@@ -1,10 +1,15 @@
 import { cookies } from "next/headers";
 import "server-only";
-import { AUTH_COOKIE } from "../constants/auth-cookie.constant";
+import {
+  AUTH_COOKIE,
+  AUTH_COOKIE_VERCEL,
+} from "../constants/auth-cookie.constant";
 import { decode, JWT } from "next-auth/jwt";
 
 export async function getAuthHeaders() {
-  const tokenCookie = cookies().get(AUTH_COOKIE)?.value;
+  const tokenCookie =
+    cookies().get(AUTH_COOKIE)?.value ||
+    cookies().get(AUTH_COOKIE_VERCEL)?.value;
   let JWT: JWT | null = null;
   try {
     JWT = await decode({
